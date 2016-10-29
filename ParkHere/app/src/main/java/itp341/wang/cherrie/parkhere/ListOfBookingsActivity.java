2ff98d2a294d45d746a8ce34f858fd2a2ba71e87
Glarence Zhao <glarencez@gmail.com>
2016-10-28 20:37:04 -0700

    Added hovering api dependencies and created ListOfListings and ListOfBookings activities

---
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/ListOfBookingsActivity.java
@@ -0,0 +1,27 @@
+package itp341.wang.cherrie.parkhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by glarencezhao on 10/27/16.
+ */
+
+public class ListOfBookingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_bookings);
+
+        initialize();
+        listeners();
+    }
+
+    private void initialize(){
+    }
+
+    private void listeners(){
+
+    }
+}
---
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/ListOfListingsActivity.java
@@ -0,0 +1,18 @@
+package itp341.wang.cherrie.parkhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by glarencezhao on 10/28/16.
+ */
+
+public class ListOfListingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_listings);
+
+    }
+}
