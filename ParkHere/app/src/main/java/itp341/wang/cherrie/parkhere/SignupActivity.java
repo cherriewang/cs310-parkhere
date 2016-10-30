69e3d01c427dba5a6d2052949a63e19b5dd4ba4b
Glarence Zhao <glarencez@gmail.com>
2016-10-30 06:22:30 -0700

    Added profile image selecting to signup

--- ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/SignupActivity.java
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/SignupActivity.java
@@ -1,12 +1,20 @@
 package itp341.wang.cherrie.parkhere;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.facebook.drawee.generic.RoundingParams;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.nguyenhoanglam.imagepicker.activity.ImagePickerActivity;
+import com.nguyenhoanglam.imagepicker.model.Image;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -19,6 +27,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import itp341.wang.cherrie.parkhere.model.User;
 
@@ -31,6 +40,10 @@ public class SignupActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText phoneEditText;
     private EditText passwordEditText;
+    private SimpleDraweeView userProfPicView;
+
+    public final static int SIGN_UP_REQUEST_CODE = 1;
+
     String URL_ADDRESS = "http://localhost:8080/signup";
     private HttpURLConnection client;
     // ImageView upload??
@@ -39,15 +52,65 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        initialize();
+        listeners();
+    }
+
+    private void writeStream(OutputStream out){
+        String output = "Hello world";
+        try {
+            out.write(output.getBytes());
+            out.flush();
+        }  catch(Exception e) {
+            String except = new String("Exception: " + e.getMessage());
+            Debug.printToast(except, getApplicationContext());
+        }
+    }
+
+    // Temp readStream function that assumes we're reading a String response back
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line = r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
+    }
+
+    private boolean isFieldsEmpty(){
+        if(!firstNameEditText.getText().toString().isEmpty()
+                && !lastNameEditText.getText().toString().isEmpty()
+                && !emailEditText.getText().toString().isEmpty()
+                && !phoneEditText.getText().toString().isEmpty()
+                && !passwordEditText.getText().toString().isEmpty())
+            return true;
+        return false;
+    }
+
+    private boolean isPasswordOK(){
+        String password = passwordEditText.getText().toString();
+        //if 10 characters and at least one special character
+        //return true
+        return false;
+    }
+
+    private void initialize(){
         firstNameEditText = (EditText) findViewById(R.id.editTextFirst);
         lastNameEditText = (EditText) findViewById(R.id.editTextLast);
         emailEditText = (EditText) findViewById(R.id.editTextEmail);
         phoneEditText = (EditText) findViewById(R.id.editTextPhone);
         passwordEditText = (EditText) findViewById(R.id.editTextPass);
         enterButton = (Button) findViewById(R.id.buttonEnter);
+        userProfPicView = (SimpleDraweeView) findViewById(R.id.userProfPicView);
+
         myUser = new User();
+    }
 
-
+    private void listeners(){
         // enterButtonListener()
         // Override onClick with an Intent to start HomeActivity if sign up is successful
         // SEND EMAIL BUTTON LISTENER
@@ -105,45 +168,24 @@ public class SignupActivity extends AppCompatActivity {
 
             }
         });
-    }
+        userProfPicView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Select Image
+                Intent intent = new Intent(SignupActivity.this, ImagePickerActivity.class);
 
-    private void writeStream(OutputStream out){
-        String output = "Hello world";
-        try {
-            out.write(output.getBytes());
-            out.flush();
-        }  catch(Exception e) {
-            String except = new String("Exception: " + e.getMessage());
-            Debug.printToast(except, getApplicationContext());
-        }
-    }
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_FOLDER_MODE, true);
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_MODE, ImagePickerActivity.MODE_SINGLE);
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_LIMIT, 1);
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_SHOW_CAMERA, true);
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_SELECTED_IMAGES, new ArrayList<Image>());
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_FOLDER_TITLE, "Albums");
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_IMAGE_TITLE, "Select an Image");
+                intent.putExtra(ImagePickerActivity.INTENT_EXTRA_IMAGE_DIRECTORY, "Camera");
 
-    // Temp readStream function that assumes we're reading a String response back
-    private String readStream(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
-        for (String line = r.readLine(); line != null; line = r.readLine()){
-            sb.append(line);
-        }
-        is.close();
-        return sb.toString();
-    }
-
-    private boolean isFieldsEmpty(){
-        if(!firstNameEditText.getText().toString().isEmpty()
-                && !lastNameEditText.getText().toString().isEmpty()
-                && !emailEditText.getText().toString().isEmpty()
-                && !phoneEditText.getText().toString().isEmpty()
-                && !passwordEditText.getText().toString().isEmpty())
-            return true;
-        return false;
-    }
-
-    private boolean isPasswordOK(){
-        String password = passwordEditText.getText().toString();
-        //if 10 characters and at least one special character
-        //return true
-        return false;
+                startActivityForResult(intent, SIGN_UP_REQUEST_CODE);
+            }
+        });
     }
 
     // Handlers for return result Intents
@@ -155,6 +197,13 @@ public class SignupActivity extends AppCompatActivity {
                 // Return from HomeActivity
             }
             break;
+        }
+
+        if(requestCode == SIGN_UP_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            ArrayList<Image> images = data.getParcelableArrayListExtra(ImagePickerActivity.INTENT_EXTRA_SELECTED_IMAGES);
+            Bitmap image = BitmapFactory.decodeFile(images.get(0).getPath());
+            userProfPicView.setImageBitmap(image);
+            //userProfPicView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         }
     }
 }
