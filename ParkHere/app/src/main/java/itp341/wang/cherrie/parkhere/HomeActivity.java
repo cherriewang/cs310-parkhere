1562f1f6f5e1124b4eed809eb9bc3730ee68a761
Glarence Zhao <glarencez@gmail.com>
2016-10-29 19:16:23 -0700

    Implemented basic navigation drawer for HomeActivity

--- ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
@@ -2,12 +2,130 @@ package itp341.wang.cherrie.parkhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionBarOverlayLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialize.util.UIUtils;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private AccountHeader headerResult = null;
+    private Drawer navDrawer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Setup Navigation Drawer
+        // Handle Toolbar
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Testing Nav Drawer");
+        // Create a few sample profile
+        final IProfile profile = new ProfileDrawerItem().withName("Glarence Zhao").withEmail("glarencez@gmail.com").withIcon(R.drawable.placeholder_prof_pic);
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(true)
+                .withHeaderBackground(R.drawable.header)
+                .withSelectionListEnabledForSingleProfile(false)
+                .addProfiles(
+                        profile
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
+        //Create the drawer
+        navDrawer = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Home").withIdentifier(1), //withIcon(FontAwesome.Icon.faw_home) before withIdentifier
+                        new PrimaryDrawerItem().withName("Free Play").withIdentifier(2),
+                        new PrimaryDrawerItem().withName("Custom").withIdentifier(3),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName("Settings").withIdentifier(4),
+                        new SecondaryDrawerItem().withName("Help").withIdentifier(5)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null) {
+                            //drawerItem.getIdentifier() == 1
+                            //drawerItem.getIdentifier() == 2
+                            //drawerItem.getIdentifier() == 3
+                            //drawerItem.getIdentifier() == 4
+                            //drawerItem.getIdentifier() == 5
+                        }
+
+                        return false;
+                    }
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        // set the selection to the item with the identifier 5
+        if (savedInstanceState == null) {
+            navDrawer.setSelection(1, false);
+        }
+
+        //set the back arrow in the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+        initialize();
+        listeners();
+    }
+
+    private void initialize(){
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = navDrawer.saveInstanceState(outState);
+        //add the values which need to be saved from the accountHeader to the bundle
+        outState = headerResult.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle the click on the back arrow click
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (navDrawer != null && navDrawer.isDrawerOpen()) {
+            navDrawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void listeners(){
+
     }
 }
