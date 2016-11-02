28c326f693e3f8976de37023982dbd99ec6118bb
Irfan Faizullabhoy <supercurry911@gmail.com>
2016-11-02 00:01:15 -0700

    incomplete login

--- ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/LoginActivity.java
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/LoginActivity.java
@@ -1,6 +1,7 @@
 package itp341.wang.cherrie.parkhere;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +9,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import itp341.wang.cherrie.parkhere.model.User;
+
 public class LoginActivity extends AppCompatActivity {
 
     private Button enterButton;
     private EditText emailEditText;
     private EditText passwordEditText;
     private TextView forgotPassword;
+    private User candidateUser;
+    private User myUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +44,29 @@ public class LoginActivity extends AppCompatActivity {
         enterButton = (Button) findViewById(R.id.buttonEnter);
     }
 
+    private boolean checkUser(User cUser, int hashedPassword) {
+        if (cUser.getmHashedPassword() == hashedPassword) {
+            ((ParkHereApplication) this.getApplication()).setMyUser(cUser);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private void listeners(){
         // ENTER BUTTON LISTENER
         enterButton.setOnClickListener(new View.OnClickListener(){
             @Override
             //On click function
             public void onClick(View view) {
-                boolean validUser = true;
-                // verify email + password, return bool
+                boolean validUser = false;
+
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                int hashedPassword = password.hashCode();
+
+                UserLoginTask t = new UserLoginTask(email, hashedPassword);
+                t.execute((Void) null);
 
                 if (validUser){
                     // Intent to start HomeActivity
@@ -79,4 +106,44 @@ public class LoginActivity extends AppCompatActivity {
             break;
         }
     }
-}
+
+    /**
+     * Represents an asynchronous, multi-threaded login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final int mHashedPassword;
+        private boolean authenticationStatus = true;
+        UserLoginTask(String username, int password) {
+            mEmail = username;
+            mHashedPassword = password;
+            authenticationStatus = doInBackground((Void) null);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //A firebaseError will occur whenever the authentication fails.
+//            ref = new Firebase("https://anchronize.firebaseio.com");
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("users").child(mEmail.replace(".", "%2E"));
+
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    candidateUser = dataSnapshot.getValue(User.class);
+                    checkUser(candidateUser, mHashedPassword);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // ...
+                }
+            });
+
+            return authenticationStatus;
+
+    }
+}}
