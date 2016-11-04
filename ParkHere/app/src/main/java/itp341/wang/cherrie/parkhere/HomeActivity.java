33fd25f888a5b3f36956402ce9769721c1f49855
Glarence Zhao <glarencez@gmail.com>
2016-11-03 21:14:24 -0700

    removed useless import statements

--- ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
@@ -1,7 +1,5 @@
 package itp341.wang.cherrie.parkhere;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,11 +17,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
