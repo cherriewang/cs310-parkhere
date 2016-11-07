dcfeb1e1a652261b6c1eae40137835bff7fc2b74
Irfan Faizullabhoy <supercurry911@gmail.com>
2016-11-07 00:32:04 -0800

    tests, eight or so

--- ParkHere/app/src/test/java/itp341/wang/cherrie/parkhere/ExampleUnitTest.java
+++ ParkHere/app/src/test/java/itp341/wang/cherrie/parkhere/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package itp341.wang.cherrie.parkhere;
 
 import org.junit.Test;
 
+import itp341.wang.cherrie.parkhere.model.Booking;
+import itp341.wang.cherrie.parkhere.model.Listing;
+import itp341.wang.cherrie.parkhere.model.Review;
+import itp341.wang.cherrie.parkhere.model.User;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +16,102 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void addBooking_isCorrect() throws Exception {
+        User user = new User();
+        Booking booking1 = new Booking(new Listing());
+        Booking booking2 = new Booking(new Listing());
+        user.appendBooking(booking1);
+        user.appendBooking(booking2);
+        assertEquals(user.getmBookings().size(), 2);
     }
+
+    @Test
+    public void addListing_isCorrect() throws Exception {
+        User user = new User();
+        Listing listing1 = new Listing();
+        Listing listing2 = new Listing();
+        user.appendListing(listing1);
+        user.appendListing(listing2);
+        assertEquals(user.getmListings().size(), 2);
+    }
+
+    @Test
+    public void RatingCorrect() throws Exception {
+        User user = new User();
+        Listing listing1 = new Listing();
+        Review review1 = new Review();
+        review1.setOwnerRating(5);
+        listing1.addReview(review1);
+        user.appendListing(listing1);
+        assertEquals(user.getAverageRating(), 5.0, 0);
+    }
+
+    @Test
+    public void AvgRatingCorrect1() throws Exception {
+        User user = new User();
+        Listing listing1 = new Listing();
+        Review review1 = new Review();
+        review1.setOwnerRating(5);
+        Review review2 = new Review();
+        review2.setOwnerRating(4);
+        listing1.addReview(review1);
+        listing1.addReview(review2);
+        user.appendListing(listing1);
+        assertEquals(user.getAverageRating(), 4.5, 0);
+    }
+    @Test
+    public void AvgRatingCorrect2() throws Exception {
+        User user = new User();
+        Listing listing1 = new Listing();
+        Review review1 = new Review();
+        review1.setOwnerRating(5);
+        Listing listing2 = new Listing();
+        Review review2 = new Review();
+        review2.setOwnerRating(4);
+        listing1.addReview(review1);
+        listing2.addReview(review2);
+        user.appendListing(listing1);
+        user.appendListing(listing2);
+        assertEquals(user.getAverageRating(), 4.5, 0);
+    }
+    @Test
+    public void AvgRatingCorrect3() throws Exception {
+        User user = new User();
+        Listing listing1 = new Listing();
+        user.appendListing(listing1);
+        assertEquals(user.getAverageRating(), 0, 0);
+    }
+
+    @Test
+    public void LatestReviewCorrect() throws Exception {
+        Listing listing = new Listing();
+        Review review1 = new Review();
+        review1.setReviewText("first review");
+        Review review2 = new Review();
+        review2.setReviewText("second review");
+        Review review3 = new Review();
+        review3.setReviewText("third review");
+        listing.addReview(review1);
+        listing.addReview(review2);
+        listing.addReview(review3);
+        assertEquals(listing.getLatestReview().getReviewText(), "third review");
+    }
+
+    @Test
+    public void ListingToBookingPrice() throws Exception {
+        Listing listing = new Listing();
+        listing.setPrice(1.0);
+        Booking booking = new Booking(listing);
+        assertEquals(listing.getPrice(), 1.0, 0);
+    }
+
+    @Test
+    public void ListingToBookingTandem() throws Exception {
+        Listing listing = new Listing();
+        listing.setTandem(true);
+        Booking booking = new Booking(listing);
+        assertEquals(listing.isTandem(), true);
+    }
+
 }
+
