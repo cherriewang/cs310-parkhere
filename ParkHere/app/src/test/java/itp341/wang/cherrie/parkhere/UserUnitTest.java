590d1796785ae329dc058da4ec9b6313366fc933
Irfan Faizullabhoy <supercurry911@gmail.com>
2016-11-10 00:03:23 -0800

    refactoring listing and user test

--- ParkHere/app/src/test/java/itp341/wang/cherrie/parkhere/UserUnitTest.java
+++ ParkHere/app/src/test/java/itp341/wang/cherrie/parkhere/UserUnitTest.java
@@ -1,8 +1,6 @@
 package itp341.wang.cherrie.parkhere;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
+import org.junit.Before;
 import org.junit.Test;
 
 import itp341.wang.cherrie.parkhere.model.Booking;
@@ -10,14 +8,15 @@ import itp341.wang.cherrie.parkhere.model.Listing;
 import itp341.wang.cherrie.parkhere.model.Review;
 import itp341.wang.cherrie.parkhere.model.User;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class UserUnitTest {
+
     @Test
     public void addBooking_isCorrect() throws Exception {
         User user = new User();
@@ -83,21 +82,6 @@ public class ExampleUnitTest {
         Listing listing1 = new Listing();
         user.appendListing(listing1);
         assertEquals(user.getAverageRating(), 0, 0);
-    }
-
-    @Test
-    public void LatestReviewCorrect() throws Exception {
-        Listing listing = new Listing();
-        Review review1 = new Review();
-        review1.setReviewText("first review");
-        Review review2 = new Review();
-        review2.setReviewText("second review");
-        Review review3 = new Review();
-        review3.setReviewText("third review");
-        listing.addReview(review1);
-        listing.addReview(review2);
-        listing.addReview(review3);
-        assertEquals(listing.getLatestReview().getReviewText(), "third review");
     }
 }
 
