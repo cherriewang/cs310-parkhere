128ac0c9414671c8b8c5f8db490e8eb1be1f17e0
Glarence Zhao <glarencez@gmail.com>
2016-11-17 21:23:06 -0800

    fixed mylocation so permission should always be requested in order to enable my location

--- ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
+++ ParkHere/app/src/main/java/itp341/wang/cherrie/parkhere/HomeActivity.java
@@ -152,8 +152,9 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     private ArrayList<Listing> allListingsToDisplay = new ArrayList<>();
     private HashMap<Marker, Listing> markerListingHashmap = new HashMap<>();
 
-    private int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
-    private int CREATE_LISTING_REQUEST_CODE = 2;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    private final int CREATE_LISTING_REQUEST_CODE = 2;
+    private final int PERMISSION_REQUEST_CODE = 3;
 
     public final static String USER_PROFILE_INTENT_KEY = "Sending user object to detail user profile activity";
 
@@ -689,20 +690,32 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     protected void enableMyLocation() {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+                ActivityCompat.requestPermissions(HomeActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+            }
+            else
+                mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            mMap.setMyLocationEnabled(true);
+                        }
+                    }
+                }
+                break;
             }
         }
-        mMap.setMyLocationEnabled(true);
     }
 
     LocationRequest mLocationRequest;
